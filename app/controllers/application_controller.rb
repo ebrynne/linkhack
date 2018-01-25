b980aebe553b124a5059490ddfe4908d311bce71
Dirk Zittersteyn <dirk@hackerone.com>
2018-01-25 11:49:43 +0100

    Refactor appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,38 +1,35 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def redirector
-    # todo: holy shit this could be cleaned up a lot. As soon as I have some free time.
-    short, args = request.fullpath[1..-1].split('/', 2).reject { |s| s.empty? }
+    fullpath = request.fullpath[1..-1]
+    short, args = fullpath.split('/', 2).reject(&:empty?)
+
     link = Link.find_by_shortlink(short)
-    if link
+
+    if link.present?
       url = link.url
-      if !link.argsstr.empty? && args
-        url = url + link.argsstr.sub('%s', args)
-      end
+
+      url += link.argsstr.sub('%s', args) if !link.argsstr.empty? && args
+
       redirect_to url
-    else
-      if RegexLink.all.map do |rlink|
-        match = /#{rlink.shortlink}/.match(request.fullpath[1..-1])
-        if match
-          url = rlink.url
-          args = if match.length > 1 then match[1] else match[0] end
-          unless args.empty?
-            unless rlink.argsstr.empty?
-              url = rlink.argsstr.sub('%s', args)
-            else
-              url = url + args
-            end
-          end
-          redirect_to url
-        end
-      end.compact.empty?
-        redirect_to :controller => 'links',
-          :action => 'new',
-          :link => {:shortlink => short, :argsstr => args}
-      end
+      return
     end
+
+    link = RegexLink.all.find { |regex_link| regex_link.matches_path? fullpath }
+
+    if link.present?
+      redirect_to link.url_for(fullpath)
+      return
+    end
+
+    redirect_to(
+      controller: :links,
+      action: :new,
+      link: {
+        shortlink: short,
+        argsstr: args
+      }
+    )
   end
 end
--- app/models/regex_link.rb
+++ app/models/regex_link.rb
@@ -1,4 +1,26 @@
-class RegexLink < Link; end
+class RegexLink < Link
+  def matches_path?(path)
+    /#{shortlink}/.match(path).present?
+  end
+
+  def url_for(path)
+    regex_match = /#{shortlink}/.match(path)
+
+    target_url = url
+
+    args = regex_match.length > 1 ? regex_match[1] : regex_match[0]
+
+    return target_url if args.empty?
+
+    if argsstr.empty?
+      target_url += args
+    else
+      target_url = argsstr.sub('%s', args)
+    end
+
+    target_url
+  end
+end
 
 # == Schema Information
 #
